3951eb8deaf3ae5dd9fe64ea32a19d22cc4af250
Mohd Vasil <mohd.vasil@broctagon.com>
2018-06-07 09:01:52 +0530

    change folder name by angular-broc-taskmanagement

--- BrocSample/src/app/modeldialog/contentmodeldialog.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from '@angular/core';
-import {MatDialog} from '@angular/material';
-@Component({
-    selector: 'content-modeldialog',
-    templateUrl: 'contentmodeldialog.html',
-  })
-  export class ContentModelDialog {}
